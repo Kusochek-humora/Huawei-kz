40afc877334d195cc4a1a5b1b6db5ad3387ec164
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2023-02-16 06:38:26 +0600

    company 80% deskp+mobile

--- gulpfile.js
+++ gulpfile.js
@@ -224,6 +224,19 @@ export const buildCss = (done) => {
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('dist/css'))
 		.pipe(sync.stream());
+		gulp.src('src/scss/company.scss')
+		.pipe(plumber())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(replace('(/images/', '(../images/'))
+		.pipe(prefixer())
+		.pipe(csso())
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(sync.stream());
 	done();
 };
 
